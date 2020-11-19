f2e7d2cb30adf74ccf501e5eefdf9518f0464a8d
Luciano Molina <lucianoma63@gmail.com>
2020-11-19 19:49:24 -0300

    Planes estrellas y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './pages/inicio/navbar/navbar.component';
 import { PrincipalimageComponent } from './pages/inicio/principalimage/principalimage.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { PlanesEstrellasComponent } from './pages/inicio/planes-estrellas/planes-estrellas.component';
+import { FooterComponent } from './pages/inicio/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PrincipalimageComponent,
-    InicioComponent
+    InicioComponent,
+    PlanesEstrellasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/inicio/planes-estrellas/planes-estrellas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planes-estrellas',
+  templateUrl: './planes-estrellas.component.html',
+  styleUrls: ['./planes-estrellas.component.css']
+})
+export class PlanesEstrellasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
