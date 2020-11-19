1bef853ce2cd93bee0e0fe4f150a83d66f8fe965
yuNNNu <nico.pobletem@alumnos.duoc.cl>
2020-11-19 19:58:18 -0300

    se arreglan conflictos y se agrega componente planes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './pages/inicio/navbar/navbar.component';
 import { PrincipalimageComponent } from './pages/inicio/principalimage/principalimage.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
-<<<<<<< HEAD
-import { BenefitsComponent } from './pages/inicio/benefits/benefits.component';
-=======
 import { PlanesEstrellasComponent } from './pages/inicio/planes-estrellas/planes-estrellas.component';
+import { BenefitsComponent } from './pages/inicio/benefits/benefits.component';
 import { FooterComponent } from './pages/inicio/footer/footer.component';
->>>>>>> f2e7d2cb30adf74ccf501e5eefdf9518f0464a8d
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PlanesComponent } from './pages/planes/planes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PrincipalimageComponent,
-    InicioComponent,
-<<<<<<< HEAD
-    BenefitsComponent
-=======
     PlanesEstrellasComponent,
-    FooterComponent
->>>>>>> f2e7d2cb30adf74ccf501e5eefdf9518f0464a8d
+    BenefitsComponent,
+    FooterComponent,
+    InicioComponent,
+    PlanesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/planes/planes.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlanesComponent } from './planes.component';
+
+describe('PlanesComponent', () => {
+  let component: PlanesComponent;
+  let fixture: ComponentFixture<PlanesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlanesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlanesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
