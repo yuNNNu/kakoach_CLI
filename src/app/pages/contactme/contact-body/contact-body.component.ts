3866ab8bc456e3b5bd88df0a69f0e6570200804d
Luciano Molina <lucianoma63@gmail.com>
2020-12-30 20:31:28 -0300

    validacion mail y user

--- src/app/pages/contactme/contact-body/contact-body.component.ts
+++ src/app/pages/contactme/contact-body/contact-body.component.ts
@@ -22,55 +22,92 @@ export class ContactBodyComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    /*=============================================
+    VALIDAR FORMULARIO
+    =============================================*/
+
+    (function () {
+      'use strict';
+      window.addEventListener('load', function () {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+
   }
 
   onSubmit(f: NgForm) {
 
-    Swal.fire({
-      title: '¿Está seguro de realizar esta acción?',
-      text: 'Si no lo está... Puede cancelar esta acción!, asegúrese que el correo sea válido',
-      icon: 'question',
-      showCancelButton: true,
-      confirmButtonText: 'Si',
-      cancelButtonText: 'No'
-    }).then((result) => {
+    var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
-      if (result.isConfirmed) {
-        this._contact.contactMe(this.datosContactMe)
-          .subscribe(res => {
-            if (res["status"] !== 200) {
 
-              Swal.fire(
-                'Error al enviar el correo!',
-                'Ha existido un error del servidor, no ha sido posible enviar el correo.',
-                'error'
-              )
 
-            } else {
+    if (this.datosContactMe["nombre"] == "" || this.datosContactMe["apellido"] == "" || this.datosContactMe["mail"] == "" || this.datosContactMe["menaje"] == "") {
 
-              Swal.fire({
+      return
 
-                title: 'Mensaje enviado',
-                text: 'Su mensaje será respondido lo antes posible, gracias por la espera!.',
-                icon: 'success',
-                confirmButtonText: 'OK!'
-              }).then((result) => {
-                window.location.href = "/";
+    } else {
+      var esValido = expReg.test(this.datosContactMe["mail"]);
+      if (!esValido) { return }
+      else {
+        Swal.fire({
+          title: '¿Está seguro de realizar esta acción?',
+          text: 'Si no lo está... Puede cancelar esta acción!, asegúrese que el correo sea válido',
+          icon: 'question',
+          showCancelButton: true,
+          confirmButtonText: 'Si',
+          cancelButtonText: 'No'
+        }).then((result) => {
+
+          if (result.isConfirmed) {
+            this._contact.contactMe(this.datosContactMe)
+              .subscribe(res => {
+                if (res["status"] !== 200) {
+
+                  Swal.fire(
+                    'Error al enviar el correo!',
+                    'Ha existido un error del servidor, no ha sido posible enviar el correo.',
+                    'error'
+                  )
+
+                } else {
+
+                  Swal.fire({
+
+                    title: 'Mensaje enviado',
+                    text: 'Su mensaje será respondido lo antes posible, gracias por la espera!.',
+                    icon: 'success',
+                    confirmButtonText: 'OK!'
+                  }).then((result) => {
+                    window.location.href = "/";
+                  })
+
+                }
               })
+          } else {
 
-            }
-          })
-      }else{
+            Swal.fire(
+              'Mensaje no enviado',
+              'Acción cancelada!.',
+              'error'
+            )
 
-        Swal.fire(
-                'Mensaje no enviado',
-                'Acción cancelada!.',
-                'error'
-              )
+          }
+
+        })
 
       }
-
-    })
+    }
 
 
   }
--- src/app/pages/user/crearusuario/crearusuario.component.ts
+++ src/app/pages/user/crearusuario/crearusuario.component.ts
@@ -3,6 +3,7 @@ import { BorderFooterComponent } from '../../herramientas/border-footer/border-f
 import { NgForm } from '@angular/forms';
 import Swal from 'sweetalert2'
 import { UserService } from '../../../services/usuario/user.service'
+import { $ } from 'protractor';
 @Component({
   selector: 'app-crearusuario',
   templateUrl: './crearusuario.component.html',
@@ -19,46 +20,104 @@ export class CrearusuarioComponent implements OnInit {
       nombre: null,
       apellido: null,
       mail: null,
-      password: null
+      password: null,
+      password1: null
     }
 
   }
 
   ngOnInit(): void {
-    this.user.GetUsers()
-      .subscribe(res => {
-        this.allUser = res["data"]
-        // RECIBE USERS
-      })
+    /*=============================================
+    VALIDAR FORMULARIO
+    =============================================*/
+
+    (function () {
+      'use strict';
+      window.addEventListener('load', function () {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+
 
   }
+
+
+
   onSubmit(f: NgForm) {
 
-    this.user.create(this.listaUsuario)
-      .subscribe(res => {
-        if (res["status"] !== 200) {
+    var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
+
+
+    if (this.listaUsuario["nombre"] == "" || this.listaUsuario["apellido"] == "" || this.listaUsuario["mail"] == "" || this.listaUsuario["password"] == "" || this.listaUsuario["password1"] == "") {
+
+      return
+
+    } else {
+      var esValido = expReg.test(this.listaUsuario["mail"]);
+      if (!esValido) {
+        Swal.fire(
+          'Formato de email invalido!',
+          'Intenté nuevamente.',
+          'error'
+        )
+      }
+      else {
+        console.log("En on submit", this.listaUsuario)
+        if (this.listaUsuario["password"] === this.listaUsuario["password1"]) {
+          this.user.create(this.listaUsuario)
+            .subscribe(res => {
+              if (res["status"] !== 200) {
+
+                Swal.fire(
+                  'Error al crear cuenta!',
+                  'Intentélo más tarde.',
+                  'error'
+                )
+
+
+
+              } else {
+
+                Swal.fire({
+                  title: 'Cuenta creada con éxito',
+                  text: 'Se le ha enviado un mail para confirmar la verificación de usuario',
+                  icon: 'success',
+                  confirmButtonText: 'OK!'
+                }).then((result) => {
+                  window.location.href = "/";
+                })
+              }
+            })
+        } else {
           Swal.fire(
-            'Error al crear cuenta!',
-            'Intentélo más tarde.',
+            'Contraseñas no coinciden!',
+            'Intenté nuevamente.',
             'error'
           )
-
-
-
-        } else {
-
-          Swal.fire({
-             title: 'Cuenta creada con éxito',
-            text: 'Se le ha enviado un mail para confirmar la verificación de usuario',
-            icon: 'success',
-            confirmButtonText: 'OK!'
-          }).then((result) => {
-            window.location.href = "/";
-          })
+          return
         }
-      })
-      
+
+      }
+
+
+    }
+
+
+
+
+
   }
 }
 
